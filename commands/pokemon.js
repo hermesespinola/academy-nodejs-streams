b83d2e34cd313b04dafd102555a76dcad0e6f981
Hermes Espinola <hermes.espinola@gmail.com>
2021-07-19 23:37:54 -0600

    filter pokemon by height and weight

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -4,12 +4,16 @@ const PokeAPI = require('../PokeAPIClient');
 const awaitStream = require('../streams/awaitStream');
 const filterStream = require('../streams/filterStream');
 
-async function* pokemonIterator() {
-  yield* await PokeAPI.fetchPokemonList();
+async function* pokemonIterator(verbose) {
+  const pokemonList = await PokeAPI.fetchPokemonList();
+  if (verbose) {
+    console.log('Processing', pokemonList.length, 'pokemons');
+  }
+  yield* pokemonList;
 }
 
-function pokemonCommand({ types, moves, abilities }) {
-  // Add filters
+function pokemonCommand({ types, moves, abilities, height, width, verbose }) {
+  // Add array filters
   const filters = [];
   if (types) {
     filters.push(filterStream((pokemon) =>
@@ -27,10 +31,26 @@ function pokemonCommand({ types, moves, abilities }) {
     ))
   }
 
+  // Add expr filters
+  if (height) {
+    const [op, h] = height;
+    const matchesHeight = Function('height', `return height ${op ?? '==='} ${h}`);
+    filters.push(filterStream((pokemon) =>
+      matchesHeight(pokemon.height)
+    ))
+  }
+  if (width) {
+    const [op, w] = width;
+    const matchesWidth = Function('width', `return width ${op ?? '==='} ${w}`);
+    filters.push(filterStream((pokemon) => 
+      matchesWidth(pokemon.width)
+    ));
+  }
+
   pipeline(
-    pokemonIterator(),
+    pokemonIterator(verbose),
     awaitStream(PokeAPI.fetchPokemonInfo),
-    multipipe(filters),
+    ...filters,
     new Writable({
       objectMode: true,
       async write(pokemon, _, callback) {
